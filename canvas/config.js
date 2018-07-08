b3ffcdec157cac9f31bd2f6357a411c534362bdc
HoMoonKi <hewenqi96@gmail.com>
2018-07-08 07:53:16 -0400

    Update

--- canvas/config.js
+++ canvas/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   url: 'mongodb://dummy:password@ds117109.mlab.com:17109',
-  dbName: 'test-db'
+  dbName: 'test-db',
+  collection: 'tabsession'
 }
--- canvas/server.js
+++ canvas/server.js
@@ -1,24 +1,18 @@
 const express = require('express');
-const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const { url, dbName } = require('./config');
+const { url, dbName, collection } = require('./config');
 
 const app = express();
 app.use(express.static(__dirname))
-app.use('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
-  next();
-})
 
 app.get('/data.json', async (req, res) => {
-  let db;
   try {
     client = await MongoClient.connect(url + '/' + dbName);
     assert.notEqual(client, null);
-    db = await client.db(dbName);
+    let db = await client.db(dbName);
     assert.notEqual(db, null);
-    records = await db.collection('tabsession').find().toArray();
+    records = await db.collection(collection).find().toArray();
     res.send(JSON.stringify(records));
   } catch (err) {
     console.log(err)
